c7847e7747fbe39afa9a97a6b081dcbf4ce7bd8a
Steven Logue <bitvector2@gmail.com>
2018-08-04 19:34:12 -0700

    integrated istio and deployments

--- src/main/java/org/bitvector/gateway/GatewayApp.java
+++ src/main/java/org/bitvector/gateway/GatewayApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(GatewayApp.class, args);
     }
 }
