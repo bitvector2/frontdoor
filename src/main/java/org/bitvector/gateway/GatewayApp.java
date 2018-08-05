b1f6b4f7b088240864c3942ce2380c452dd5aac7
Steven Logue <bitvector2@gmail.com>
2018-08-05 14:16:05 -0700

    switched to application.yml DSL to express Gateway behavior

--- src/main/java/org/bitvector/gateway/GatewayApp.java
+++ src/main/java/org/bitvector/gateway/GatewayApp.java
@@ -2,10 +2,8 @@ package org.bitvector.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayApp {
 
     public static void main(String[] args) {
