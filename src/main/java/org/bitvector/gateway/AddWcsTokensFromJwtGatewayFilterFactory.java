dda5b08a09e34cdd97e64ad5122e42b2af26e319
Steven Logue <bitvector2@gmail.com>
2018-08-08 19:10:39 -0700

    syncpoint

---
+++ src/main/java/org/bitvector/gateway/AddWcsTokensFromJwtGatewayFilterFactory.java
@@ -0,0 +1,63 @@
+package org.bitvector.gateway;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class AddWcsTokensFromJwtGatewayFilterFactory extends AbstractGatewayFilterFactory<AddWcsTokensFromJwtGatewayFilterFactory.Config> {
+    public static final String HEADER_NAME = "headerName";
+    private static final Log log = LogFactory.getLog(AddWcsTokensFromJwtGatewayFilterFactory.class);
+
+    public AddWcsTokensFromJwtGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(HEADER_NAME);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        log.info("AddWcsTokensFromJwtGatewayFilterFactory.apply() was called");
+
+        return (exchange, chain) -> {
+            String headerName = config.getHeaderName();
+
+            String headerValue = exchange
+                    .getRequest()
+                    .getHeaders()
+                    .getFirst(headerName);
+
+            String jwt = Util.getJwt(headerName, headerValue);
+
+            ServerHttpRequest request = exchange
+                    .getRequest()
+                    .mutate()
+                    .header("WCS_TOKEN", jwt)
+                    .header("WCS_SECURE_TOKEN", jwt)
+                    .build();
+
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+    public static class Config {
+        private String headerName;
+
+        public String getHeaderName() {
+            return headerName;
+        }
+
+        public void setHeaderName(String headerName) {
+            this.headerName = headerName;
+        }
+    }
+}
