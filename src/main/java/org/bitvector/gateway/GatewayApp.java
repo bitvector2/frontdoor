e47d8140ab3b6b45b09d27dbe0ee5505344a0731
Steven Logue <bitvector2@gmail.com>
2018-08-08 21:28:33 -0700

    Added initial support for caffeine caching

--- src/main/java/org/bitvector/gateway/GatewayApp.java
+++ src/main/java/org/bitvector/gateway/GatewayApp.java
@@ -2,8 +2,10 @@ package org.bitvector.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GatewayApp {
 
     public static void main(String[] args) {
